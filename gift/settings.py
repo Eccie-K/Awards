c7c4add4f512c489f8c21f0a9b61a74388b66824
esther <estherkc100@gmail.com>
2019-09-07 22:27:04 +0300

    added configurations of the applications in settings.py file

--- gift/settings.py
+++ gift/settings.py
@@ -1,6 +1,9 @@
 
 import os
 from decouple import config
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -114,9 +117,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
